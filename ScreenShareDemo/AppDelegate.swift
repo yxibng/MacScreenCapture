90e5b39ea143dabafaa94e3dc0d4b67afad3dda7
yaoxiaobing <xiaobing.yao@duobei.com>
2020-06-17 09:38:06 +0800

    Initial Commit

---
+++ ScreenShareDemo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ScreenShareDemo
+//
+//  Created by yxibng on 2020/6/17.
+//  Copyright © 2020 yxibng. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ScreenShareDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ScreenShareDemo
+//
+//  Created by yxibng on 2020/6/17.
+//  Copyright © 2020 yxibng. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
