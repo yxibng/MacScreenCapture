47085f4f2cce0bb20e2dd24aa00c96393c0dc2f0
yaoxiaobing <xiaobing.yao@duobei.com>
2020-06-18 16:03:20 +0800

    feat: i420 buffer

--- ScreenShareDemo/WindowShare/DbyWindowCaptureController.m
+++ ScreenShareDemo/WindowShare/DbyWindowCaptureController.m
@@ -33,7 +33,7 @@
     [super viewDidLoad];
     _taskQueue = dispatch_queue_create("queue", DISPATCH_QUEUE_SERIAL);
     _windowID = kCGNullWindowID;
-    _windowID = 265205;
+    _windowID = 269343;
     [self startTimer];
 }
 
@@ -73,16 +73,39 @@
 
 - (void)captureWindowImageFrame
 {
-    CGImageRef windowImage = CGWindowListCreateImage(CGRectNull, kCGWindowListOptionIncludingWindow, self.windowID, kCGWindowImageDefault);
     
+    //取得窗口快照
+    CGImageRef windowImage = CGWindowListCreateImage(CGRectNull, kCGWindowListOptionIncludingWindow, self.windowID, kCGWindowImageDefault);
     if (!windowImage) {
         return;
     }
     
-    CVPixelBufferRef buffer = [self pixelBufferFromCGImage:windowImage];
+//    //追加光标， 参考 https://www.coder.work/article/1297008
+//    CGImageRef imagWithCursor = [self appendMouseCursor:windowImage];
+//    CFRelease(windowImage);
+//    if (!imagWithCursor) {
+//        return;
+//    }
+//
+    
+    //做裁切
+    CGImageRef croppedImage = CGImageCreateWithImageInRect(windowImage, CGRectInfinite);
+    CFRelease(windowImage);
+    if (!croppedImage) {
+        return;
+    }
+    
+//    //写文件
+//    static int index = 0;
+//    [self writeImageWithIndex:index image:croppedImage];
+//    index++;
+    
+    
+    //转化为 buffer 并展示
+    CVPixelBufferRef buffer = [self pixelBufferFromCGImage:croppedImage];
     [self.videoView displayPixelBuffer:buffer];
     CVPixelBufferRelease(buffer);
-    CFRelease(windowImage);
+    CFRelease(croppedImage);
 }
 
 
@@ -143,22 +166,16 @@ BOOL CGImageWriteToFile(CGImageRef image, NSString *path) {
                                  (void *)data,
                                  (__bridge CFDictionaryRef _Nullable)att,
                                  &buffer);
-    return buffer;
-    
-    
-    
-    
-     CVPixelBufferRef i420Buffer;
-    
-    CVPixelBufferCreate(kCFAllocatorDefault, originalWidth, originalHeight, kCVPixelFormatType_420YpCbCr8Planar, (__bridge CFDictionaryRef _Nullable)att,&i420Buffer);
-    
     
     
     CVPixelBufferLockBaseAddress(buffer, 0);
+    int width = CVPixelBufferGetWidth(buffer);
+    int height = CVPixelBufferGetHeight(buffer);
+    
+    
+    CVPixelBufferRef i420Buffer;
+    CVPixelBufferCreate(kCFAllocatorDefault, width, height, kCVPixelFormatType_420YpCbCr8Planar, (__bridge CFDictionaryRef _Nullable)att,&i420Buffer);
     CVPixelBufferLockBaseAddress(i420Buffer, 0);
-    
-    
-    
     
     void *y_frame = CVPixelBufferGetBaseAddressOfPlane(i420Buffer, 0);
     void *u_frame = CVPixelBufferGetBaseAddressOfPlane(i420Buffer, 1);
@@ -174,11 +191,11 @@ BOOL CGImageWriteToFile(CGImageRef image, NSString *path) {
     void *rgb_stride = CVPixelBufferGetBytesPerRow(buffer);
     
     
-    BGRAToI420(rgb, rgb_stride,
+    ARGBToI420(rgb, rgb_stride,
                y_frame, stride_y,
                u_frame, stride_u,
                v_frame, stride_v,
-               originalWidth, originalHeight);
+               width, height);
     
     CVPixelBufferUnlockBaseAddress(i420Buffer, 0);
     CVPixelBufferUnlockBaseAddress(buffer, 0);
@@ -193,6 +210,63 @@ void _CVPixelBufferReleaseBytesCallback(void *releaseRefCon, const void *baseAdd
     CFRelease(data);
     
 }
+
+
+-(CGImageRef)appendMouseCursor:(CGImageRef)pSourceImage{
+    // get the cursor image
+    NSPoint mouseLoc;
+    mouseLoc = [NSEvent mouseLocation]; //get cur
+
+    NSLog(@"Mouse location is x=%d,y=%d",(int)mouseLoc.x,(int)mouseLoc.y);
+
+    // get the mouse image
+    NSImage *overlay    =   [[[NSCursor arrowCursor] image] copy];
+
+    NSLog(@"Mouse location is x=%d,y=%d cursor width = %d, cursor height = %d",(int)mouseLoc.x,(int)mouseLoc.y,(int)[overlay size].width,(int)[overlay size].height);
+
+    int x = (int)mouseLoc.x;
+    int y = (int)mouseLoc.y;
+    int w = (int)[overlay size].width;
+    int h = (int)[overlay size].height;
+    int org_x = x;
+    int org_y = y;
+
+    size_t height = CGImageGetHeight(pSourceImage);
+    size_t width =  CGImageGetWidth(pSourceImage);
+    int bytesPerRow = CGImageGetBytesPerRow(pSourceImage);
+
+    unsigned int * imgData = (unsigned int*)malloc(height*bytesPerRow);
+
+    // have the graphics context now,
+    CGRect bgBoundingBox = CGRectMake (0, 0, width,height);
+
+    CGContextRef context =  CGBitmapContextCreate(imgData, width,
+                                                  height,
+                                                  8, // 8 bits per component
+                                                  bytesPerRow,
+                                                  CGImageGetColorSpace(pSourceImage),
+                                                  CGImageGetBitmapInfo(pSourceImage));
+
+    // first draw the image
+    CGContextDrawImage(context,bgBoundingBox,pSourceImage);
+
+    // then mouse cursor
+    CGContextDrawImage(context,CGRectMake(0, 0, width,height),pSourceImage);
+
+    // then mouse cursor
+    CGContextDrawImage(context,CGRectMake(org_x, org_y, w,h),[overlay CGImageForProposedRect: NULL context: NULL hints: NULL] );
+
+
+    // assuming both the image has been drawn then create an Image Ref for that
+
+    CGImageRef pFinalImage = CGBitmapContextCreateImage(context);
+
+    CGContextRelease(context);
+    free(imgData);
+
+    return pFinalImage; /* to be released by the caller */
+}
+
 
 
 @end
